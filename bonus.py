29989e81aba468f41593ed35df2000f8dea6898c
veereshn696-alt <veereshn696@gmail.com>
2025-11-27 14:59:34 +0530

    Update bonus.py

--- bonus.py
+++ bonus.py
@@ -3,8 +3,8 @@ if len(sys.argv)!=3 :
   print("usage : python script.py <salary> <bonus>")
   exit(1)
 script=sys.argv[0]
-salary=sys.argv[1]
-bonus=sys.argv[2]
+salary=float(sys.argv[1])
+bonus=float(sys.argv[2])
 print("salary is ",salary)
 print("bonus is ",bonus)
 total=(salary * bonus/100)+salary
