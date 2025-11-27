9218fbccf1776ce92a80af5b6f1844b97be11845
veereshn696-alt <veereshn696@gmail.com>
2025-11-27 14:56:34 +0530

    Create bonus.py

---
+++ bonus.py
@@ -0,0 +1,11 @@
+import sys
+if len(sys.argv)!=3 :
+  print("usage : python script.py <salary> <bonus>")
+  exit(1)
+script=sys.argv[0]
+salary=sys.argv[1]
+bonus=sys.argv[2]
+print("salary is ",salary)
+print("bonus is ",bonus)
+total=(salary * bonus/100)+salary
+print("total salary is is ",total)
